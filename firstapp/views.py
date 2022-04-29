3bd514772f38556339f9afaf3258014759ae638b
nikorr0 <nikita.nikita170@yandex.ru>
2022-04-29 23:15:50 +0500

    without prediction class

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,7 @@
 from distutils.command.upload import upload
 from django.shortcuts import render
 from django.http import HttpResponse
+<<<<<<< Updated upstream
 #from .forms import UploadFileForm
 
 def index(request):
@@ -9,6 +10,49 @@ def index(request):
     #if request.method == "POST":
         #form = UploadFileForm(request.POST, request.FILES)
     return render(request, "index.html")
+=======
+from django.core.files.storage import default_storage
+# import cv2
+supported_formats = [".png", ".jpg", "jpeg"]
+
+def index(request):
+    # print(request.FILES)
+    # print(request.FILES['photo'])
+    if request.method == 'POST':
+        # print(request.FILES['photo'])
+        # print(request.FILES['photo'].read())
+        #print(dir(request.FILES['photo']))
+        if bool(request.FILES) == False:
+            data = {"image": "False"}
+            return render(request, "index.html", context=data)
+
+        image_format = list(request.FILES['photo'].name)
+        image_format = image_format[-4] + image_format[-3] + image_format[-2] + image_format[-1]
+        #print(image_format)
+        #image_format = "jpg"
+
+        if image_format not in supported_formats:
+            data = {"image": "False"}
+            return render(request, "index.html", context=data)
+        
+            
+        with open(r"static/images/apple_saved.jpg", "wb") as f:
+            f.write(request.FILES['photo'].read())
+            f.close()
+        # with default_storage.open("tmp/apple_saved.jpg", 'wb') as destination:
+            # for chunk in request.FILES['photo'].chunks():
+                # destination.write(chunk)
+        # path = "apple_saved.jpg"
+        # cv2.imread(path)
+        data = {"image": "images/apple_saved.jpg"}
+    
+    else:
+        data = {"image": "None"}
+
+
+
+    return render(request, "index.html", context=data)
+>>>>>>> Stashed changes
 
 def predict(request):
     output = "<h2>Предсказать</h2>"
--- fruitpred/settings.py
+++ fruitpred/settings.py
@@ -31,6 +31,11 @@ ALLOWED_HOSTS = []
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
+
+#STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -121,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
