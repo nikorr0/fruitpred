28073349e627b19b6528a496403af32aee825403
nikorr0 <46054489+nikorr0@users.noreply.github.com>
2022-05-06 12:01:15 +0500

    Secret_key was deleted

--- fruitpred/settings.py
+++ fruitpred/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nju)28z=qee)$+9c8*z7%&=nh-a8sz$7+a7)1l(+%l5p=yves@'
-
+SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
